d2bcd6d6a46fb62d75bc45a71f78fa71a28a93b4
Albina Anistratenko <albuska2013@gmail.com>
2025-01-07 10:22:48 +0200

    change

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,6 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   output: "export",
-  basePath: "/template-for-partners",
   images: {
     unoptimized: true,
   },
