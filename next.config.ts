12a8a0369bf6bde843fd062d7ae78ef4776a267c
Albina Anistratenko <albuska2013@gmail.com>
2025-01-07 10:20:26 +0200

    change

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,9 @@ const nextConfig: NextConfig = {
   /* config options here */
   output: "export",
   basePath: "/template-for-partners",
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
